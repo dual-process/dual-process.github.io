cd905badd30a557a72b17421fd1d40a602c365e6
g-luo <graceluo@berkeley.edu>
2025-03-10 17:38:58 -0700

    Add all examples

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -35,12 +35,48 @@ function changeImage(id) {
 
   const image = document.getElementById(id);
   const imageSrc = image.src;
-  const imagePath = imageSrc.split("/");
-  let newImagePath = imagePath.slice(0, -1).join("/") + "/";
+  let imagePath = imageSrc.split("/");
+  imagePath = imagePath.slice(0, -1).join("/") + "/";
 
-  console.log(image);
-  console.log(imageSrc);
+  image.src = imagePath + path0 + "_" + path1 + ".png";
+}
 
-  newImagePath += path0 + "_" + path1 + ".png";
-  image.src = newImagePath;
+function changeVisualPrompt(id) {
+  const selectParent = document.getElementById('select-' + id);
+  if (selectParent === null) {
+      return ;
+  }
+  let path0 = selectParent.getElementsByTagName('select')[0].selectedOptions[0].id;
+  let path1 = selectParent.getElementsByTagName('select')[1].selectedOptions[0].id;
+
+  const image0 = document.getElementById(id + '0');
+  const image1 = document.getElementById(id + '1');
+  
+  const imageSrc = image0.src;
+  let imagePath = imageSrc.split("/");
+  imagePath = imagePath.slice(0, -1).join("/") + "/";
+
+  image0.src = imagePath + id + "_" + path1 + ".png";
+  image1.src = imagePath + path0 + "_" + path1 + ".png";
+
+  return { path0, path1, imagePath };
+}
+
+function changeVisualPromptOverlay(id) {
+  const selectParent = document.getElementById('select-' + id);
+  if (selectParent === null) {
+      return ;
+  }
+
+  const { path0, path1, imagePath } = changeVisualPrompt(id);
+
+  let shouldOverlay = selectParent.getElementsByTagName('select')[2].selectedOptions[0].id;
+  const image2 = document.getElementById(id + '2');
+  image2.src = imagePath + "overlay" + "_" + path1 + ".png";
+
+  if (shouldOverlay === 'yes') {
+    image2.style.opacity = '1';
+  } else {
+    image2.style.opacity = '0';
+  }
 }
